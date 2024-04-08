1dd1ab6fdf441fa97d396b19b7bd1552117d932b
AFCMS <afcm.contact@gmail.com>
2024-04-08 15:03:29 +0200

    Static sprite

---
+++ src/utils/display_sprite_static.cpp
@@ -0,0 +1,21 @@
+#include "display_sprite_static.hpp"
+
+namespace utils
+{
+    namespace display
+    {
+        SpriteStatic::SpriteStatic(int16_t _x, int16_t _y, const uint8_t *_bitmap, int16_t _w, int16_t _h)
+        {
+            x = _x;
+            y = _y;
+            bitmap = _bitmap;
+            w = _w;
+            h = _h;
+        };
+
+        void SpriteStatic::drawFrame(DisplaySystem *displaySystem)
+        {
+            displaySystem->drawBitmap(x, y, bitmap, w, h);
+        };
+    };
+};
---
+++ src/utils/display_sprite_static.hpp
@@ -0,0 +1,18 @@
+#include "display.hpp"
+
+namespace utils
+{
+    namespace display
+    {
+        class SpriteStatic : public Node2D
+        {
+        private:
+            const uint8_t *bitmap;
+            int16_t w, h;
+
+        public:
+            SpriteStatic(int16_t _x, int16_t _y, const uint8_t *bitmap, int16_t _w, int16_t _h); // Update the parameter type accordingly
+            void drawFrame(DisplaySystem *displaySystem) override;
+        };
+    };
+};

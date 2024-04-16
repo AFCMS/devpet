c18a363137d3e1c722c06f6a90f2a1926445b3ee
AFCMS <afcm.contact@gmail.com>
2024-04-16 12:42:21 +0200

    Add documentation to `Text` class

--- src/utils/display/display_text.hpp
+++ src/utils/display/display_text.hpp
@@ -32,19 +32,68 @@ namespace utils
             static const unsigned char DEFAULT_CHAR_LIMIT = -1;
 
         public:
+            /**
+             * @brief Create a Text node with an empty base string
+             */
             Text();
+
+            /**
+             * @brief Create a Text node with a given base string
+             */
             Text(String _text);
+
+            /**
+             * @brief Get the node's text
+             */
             String getText();
+
             /**
              * @brief Set the node's text
              */
             void setText(String _text);
+
+            /**
+             * @brief Get the size of the text to be displayed.
+             *
+             * 1 is default 6x8, 2 is 12x16, 3 is 18x24, etc.
+             *
+             * See the `Adafruit_GFX` library for more information.
+             */
             unsigned char getFontSize();
+
+            /**
+             * @brief Set the size of the text to be displayed.
+             *
+             * 1 is default 6x8, 2 is 12x16, 3 is 18x24, etc.
+             *
+             * See the `Adafruit_GFX` library for more information.
+             */
             void setFontSize(unsigned char _size);
+
+            /**
+             * @brief Get the maximum number of characters that can be displayed.
+             */
             unsigned char getCharsMax();
+
+            /**
+             * @brief Set the maximum number of characters that can be displayed.
+             *
+             * If the text is centered the text will be displayed in the center of the space reserved by the max chars value.
+             */
             void setCharsMax(unsigned char _charsMax);
+
+            /**
+             * @brief Check if the text should be centered
+             */
             bool isCentered();
+
+            /**
+             * @brief Set whether the text should be centered.
+             *
+             * If the text is centered, the text will be displayed in the center of the space reserved by the max chars value.
+             */
             void setCentered(bool _centered);
+
             void drawFrame(DisplaySystem *displaySystem) override;
         };
     }
